68468a28eaa2ec2eb4256925eb2eeabeef916dd0
Stalyn0101 <yixon11@gmail.com>
2025-02-26 13:40:04 -0500

    Clase Examen programacion

---
+++ src/main/java/ec/edu/espoch/gestionpersonas/GestionPersonas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.gestionpersonas;
+
+/**
+ *
+ * @author USER
+ */
+public class GestionPersonas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
